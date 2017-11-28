d1e8dab7dfb1a4abb7e4577b75b9b9aacad35a3c
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2017-11-28 18:06:13 +0100

    Fix ENDPOINT in send.sh

--- send.sh
+++ send.sh
@@ -2,6 +2,6 @@
 
 NAMESPACE=$1
 COLORS=$2
-ENDPOINT="http://localhost:8123"
+ENDPOINT="http://localhost:8123/${NAMESPACE}"
 echo "${COLORS}" | tr ' ' ',' | tr -d '#' | xargs -I{} curl -XPOST -d"colors={}" "${ENDPOINT}"
 
