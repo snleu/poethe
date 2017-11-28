b047fc407a5526ff82593e37d1ad1ac1bcde568e
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2017-11-28 18:05:18 +0100

    Update send.sh

--- send.sh
+++ send.sh
@@ -1,6 +1,7 @@
 #/usr/bin/env bash
 
-COLORS=$1
-ENDPOINT="https://localhost:8123"
-echo "${COLORS}" | tr ' ' ',' | tr -d '#' | xargs -I{} curl -XPOST -d"colors={}" "localhost:8123"
+NAMESPACE=$1
+COLORS=$2
+ENDPOINT="http://localhost:8123"
+echo "${COLORS}" | tr ' ' ',' | tr -d '#' | xargs -I{} curl -XPOST -d"colors={}" "${ENDPOINT}"
 
